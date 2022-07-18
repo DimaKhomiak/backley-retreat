a6ad26ae5f8e44cf215efd81eddf1c24eda51c57
DimaKhomiak <84566704+DimaKhomiak@users.noreply.github.com>
2022-07-18 11:37:37 +0300

    Update nationalMode.js

--- js/nationalMode.js
+++ js/nationalMode.js
@@ -1,52 +1,34 @@
-let input = document.querySelector("#international_PhoneNumber_countrycode")
+async function callFuncAfterTimeOut(miliSec, callFunction) {
+    await new Promise(resolve => setTimeout(resolve, miliSec));
+    return callFunction()
+}
 
-let iti = window.intlTelInput(input, {
-    nationalMode: true,
-    initialCountry: "auto",
-    geoIpLookup: function (success, failure) {
-        $.get("https://ipinfo.io?token=eb601a91986a65", function () {
-        }, "jsonp").always(function (resp) {
-            let countryCode = (resp && resp.country) ? resp.country : "us";
+async function initializeItiPlaceholder() {
+    let input = document.querySelector("#international_PhoneNumber_countrycode")
 
-            $('#G_City').val((resp && resp.city) ? resp.city : null)
-            $('#G_Country').val((resp && resp.country) ? resp.country : null)
-            $('#G_Hostname').val((resp && resp.hostname) ? resp.hostname : null)
-            $('#G_Ip').val((resp && resp.ip) ? resp.ip : null)
-            $('#G_Location').val((resp && resp.loc) ? resp.loc : null)
-            $('#G_Org').val((resp && resp.org) ? resp.org : null)
-            $('#G_Postal').val((resp && resp.postal) ? resp.postal : null)
-            $('#G_Region').val((resp && resp.region) ? resp.region : null)
-            $('#G_Timezone').val((resp && resp.timezone) ? resp.timezone : null)
+    let iti = await window.intlTelInput(input, {
+        nationalMode: true,
+        initialCountry: window.country ?? "US",
+        customPlaceholder:  await function (selectedCountryPlaceholder, selectedCountryData) {
+            console.log('window.country: ', window.country)
+            let returnData = '';
+            console.log("selectedCountryPlaceholder: ", selectedCountryPlaceholder)
+            console.log("selectedCountryData: ", selectedCountryData)
+            let mask = selectedCountryPlaceholder
+                .replace(/[0-9]/g, 'x')
+                .replace(/[()-]/g, ' ')
+            mask = selectedCountryData.dialCode + " " + mask
+            console.log("mask: ", mask)
+            $("#international_PhoneNumber_countrycode").val('');
+            $("#international_PhoneNumber_countrycode").mask("+" + mask);
+            returnData = "+" + selectedCountryData.dialCode + " " + selectedCountryPlaceholder;
+            return returnData;
+        },
+        utilsScript: "./js/utils.js?1638200991544", // just for formatting/placeholders etc
+    });
+}
 
-            console.log("resp: ", resp)
+
+callFuncAfterTimeOut(1000, initializeItiPlaceholder).then()
 
 
-            success(countryCode);
-        });
-    },
-    customPlaceholder: function (selectedCountryPlaceholder, selectedCountryData) {
-        console.log("selectedCountryPlaceholder: ", selectedCountryPlaceholder)
-        console.log("selectedCountryData: ", selectedCountryData)
-
-        let mask = selectedCountryPlaceholder
-            .replace(/[0-9]/g, 'x')
-            .replace(/[()-]/g, ' ')
-
-        mask = selectedCountryData.dialCode + " " + mask
-
-        console.log("mask: ", mask)
-
-        $("#international_PhoneNumber_countrycode").val('');
-        $("#international_PhoneNumber_countrycode").mask("+" + mask);
-
-        return "+" + selectedCountryData.dialCode + " " + selectedCountryPlaceholder;
-    },
-    utilsScript: "./js/utils.js?1638200991544", // just for formatting/placeholders etc
-});
-
-// let handleChange = function() {
-//     let text = (iti.isValidNumber()) ? "International: " + iti.getNumber() : "Please enter a number below";
-//     console.log("handleChange: ", text)
-// };
-//
-// input.addEventListener('change', handleChange);
